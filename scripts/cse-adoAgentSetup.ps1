7a8440a432735a9dce263de89cd83eba4c604c2e
Mike Smith <msmith.ausemart@gmail.com>
2023-03-04 13:11:37 +1000

    updated agent to latest version

--- scripts/cse-adoAgentSetup.ps1
+++ scripts/cse-adoAgentSetup.ps1
@@ -1,7 +1,7 @@
 param ($devOpsUri, $devOpsPAT)
 
 # Download Azure DevOps Pool Agent & extract
-wget https://vstsagentpackage.azureedge.net/agent/2.214.1/vsts-agent-win-x64-2.214.1.zip -o vsts-agent.zip
+wget https://vstsagentpackage.azureedge.net/agent/2.217.2/vsts-agent-win-x64-2.217.2.zip -o vsts-agent.zip
 New-Item -Path "C:\" -Name "agent" -ItemType "directory"
 Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD\vsts-agent.zip", "C:\agent")
 
