3e976aedcb4855484465a67057ebfa431a9b071a
James <23567798+jamesdplee@users.noreply.github.com>
2025-04-14 17:15:39 +1000

    update script and bundle for ws2025 support

--- scripts/cse-vmAEMWebAppSetup.ps1
+++ scripts/cse-vmAEMWebAppSetup.ps1
@@ -1,24 +1,30 @@
 $tempDir = [System.IO.Path]::GetTempPath()
-$dotnetHostBundleExe = "dotnet-hosting-7.0.3-win.exe"
-$dotnetHostBundleUri = "https://download.visualstudio.microsoft.com/download/pr/ff197e9e-44ac-40af-8ba7-267d92e9e4fa/d24439192bc549b42f9fcb71ecb005c0/$dotnetHostBundleExe"
+
+# Set up the environment
+$dotnetHostBundleExe = "dotnet-hosting-6.0.36-win.exe"
+$dotnetHostBundleUri = "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/6.0.36/$dotnetHostBundleExe"
 $dotnetHostBundleExePath = "$tempDir\$dotnetHostBundleExe"
 $dotnetHostBundleExeArgs = New-Object -TypeName System.Collections.Generic.List[System.String]
 $dotnetHostBundleExeArgs.Add("/quiet")
 $dotnetHostBundleExeArgs.Add("/norestart")
+
+# WebApp config
 $webappUri = "https://github.com/jamesdplee/ausemartweb.git"
 $webappSrcPath = "$tempDir\src"
-$iisPath = "c:\inetpub\wwwroot"
+$iisPath = "C:\inetpub\wwwroot"
 
 # Download/prep files
-wget $dotnetHostBundleUri -o $dotnetHostBundleExePath
-New-Item -Path $webappSrcPath -ItemType "directory"
+Invoke-WebRequest -Uri $dotnetHostBundleUri -OutFile $dotnetHostBundleExePath
+if (-Not (Test-Path $webappSrcPath)) {
+    New-Item -Path $webappSrcPath -ItemType "directory"
+}
 git clone $webappUri $webappSrcPath
 
 # Install IIS and dotnet hosting bundle
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 Start-Process -FilePath $dotnetHostBundleExePath -ArgumentList $dotnetHostBundleExeArgs -NoNewWindow -Wait -PassThru -WorkingDirectory $tempDir
 
-# Build the web application
+# Build and publish the webapp
 Set-Location $webappSrcPath
 dotnet build
 dotnet publish --output $iisPath
